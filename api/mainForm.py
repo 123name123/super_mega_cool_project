5878390e8ae4c89b8fa98da322b7d4e2d5260e68
MaximZobkov <46843651+MaximZobkov@users.noreply.github.com>
2020-02-14 22:45:06 +0300

    change_map_function_fixed

--- api/mainForm.py
+++ api/mainForm.py
@@ -2,11 +2,13 @@
 
 # Form implementation generated from reading ui file 'main.ui'
 #
-# Created by: PyQt5 UI code generator 5.9.2
+# Created by: PyQt5 UI code generator 5.13.0
 #
 # WARNING! All changes made in this file will be lost!
 
+
 from PyQt5 import QtCore, QtGui, QtWidgets
+
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -18,21 +20,13 @@ class Ui_MainWindow(object):
         self.our_map.setGeometry(QtCore.QRect(10, 10, 650, 450))
         self.our_map.setText("")
         self.our_map.setObjectName("our_map")
-        self.input = QtWidgets.QLineEdit(self.centralwidget)
-        self.input.setGeometry(QtCore.QRect(10, 510, 341, 31))
-        self.input.setObjectName("input")
-        self.search = QtWidgets.QPushButton(self.centralwidget)
-        self.search.setGeometry(QtCore.QRect(10, 550, 341, 31))
-        self.search.setObjectName("search")
-        self.label_2 = QtWidgets.QLabel(self.centralwidget)
-        self.label_2.setGeometry(QtCore.QRect(10, 470, 211, 31))
-        self.label_2.setObjectName("label_2")
-        self.chng_map = QtWidgets.QPushButton(self.centralwidget)
-        self.chng_map.setGeometry(QtCore.QRect(10, 590, 341, 31))
-        self.chng_map.setObjectName("chng_map")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(370, 510, 281, 71))
+        self.pushButton.setFlat(False)
+        self.pushButton.setObjectName("pushButton")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 674, 26))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 674, 21))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
@@ -45,7 +39,4 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.search.setText(_translate("MainWindow", "Искать"))
-        self.label_2.setText(_translate("MainWindow", "Введите адрес:"))
-        self.chng_map.setText(_translate("MainWindow", "press to change type of the map"))
-
+        self.pushButton.setText(_translate("MainWindow", "Сменить формат карты"))
