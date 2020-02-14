5dfc1cdc4cd39d73fd6a6ebd10aa16751b3607d7
123name123 <muxin-2002@mail.ru>
2020-02-14 22:10:51 +0300

    search_function

--- api/main.py
+++ api/main.py
@@ -1,11 +1,9 @@
+from mainForm import Ui_MainWindow
 import sys
-
 import requests
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QMainWindow
-
-from mainForm import Ui_MainWindow
 
 
 class Example(QMainWindow, Ui_MainWindow):
@@ -13,28 +11,40 @@ class Example(QMainWindow, Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
         self.initUI()
-        self.pushButton.clicked.connect(self.map_change_sp)
-        self.pushButton_2.clicked.connect(self.map_change_map)
-        self.pushButton_3.clicked.connect(self.map_change_sh)
 
     def initUI(self):
+        self.search.clicked.connect(self.run_search)
         self.dest_num = 1
         self.dest_list = [0.002, 0.005, 0.02, 0.05, 0.1, 0.3, 0.5, 1, 3, 5, 11, 15, 40]
         self.shir_ch = 37
         self.dol_ch = 55
         self.map = 'map'
+
+    def run_search(self):
+        toponym_to_find = self.input.text()
+        geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+        geocoder_params = {
+            "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+            "geocode": toponym_to_find,
+            "format": "json"}
+        response = requests.get(geocoder_api_server, params=geocoder_params)
+        if not response:
+            return
+        json_response = response.json()
+        toponym = json_response["response"]["GeoObjectCollection"][
+            "featureMember"][0]["GeoObject"]
+        self.shir_ch, self.dol_ch = toponym["Point"]["pos"].split()
+        self.shir_ch, self.dol_ch = float(self.shir_ch), float(self.dol_ch)
+        print(self.shir_ch, self.dol_ch)
         self.run_start()
 
-    def map_change_sp(self):
-        self.map = 'sat'
-        self.run_start()
-
-    def map_change_map(self):
-        self.map = 'map'
-        self.run_start()
-
-    def map_change_sh(self):
-        self.map = 'skl'
+    def map_chng(self):
+        if self.map == 'map':
+            self.map = 'sat'
+        elif self.map == 'sat':
+            self.map = 'skl'
+        elif self.map == 'skl':
+            self.map = 'map'
         self.run_start()
 
     def keyPressEvent(self, event):
@@ -46,22 +56,25 @@ class Example(QMainWindow, Ui_MainWindow):
             if self.dest_num >= 1:
                 self.dest_num -= 1
             self.run_start()
-        elif event.key() == Qt.Key_Up:
+        elif event.key() == Qt.Key_W:
             self.dol_ch += self.dest_list[self.dest_num]
             self.run_start()
-        elif event.key() == Qt.Key_Down:
+        elif event.key() == Qt.Key_S:
             self.dol_ch -= self.dest_list[self.dest_num]
             self.run_start()
-        elif event.key() == Qt.Key_Right:
+        elif event.key() == Qt.Key_D:
             self.shir_ch += self.dest_list[self.dest_num]
             self.run_start()
-        elif event.key() == Qt.Key_Left:
+        elif event.key() == Qt.Key_A:
             self.shir_ch -= self.dest_list[self.dest_num]
             self.run_start()
+        elif event.key() == Qt.Key_M:
+            self.map_chng()
 
     def run_start(self):
         try:
             dest = self.dest_list[self.dest_num]
+            print(dest)
             map_request = f"http://static-maps.yandex.ru/1.x/?" \
                           f"ll={self.shir_ch},{self.dol_ch}&spn" \
                           f"={dest},{dest}&l={self.map}&size=650,450"
--- api/mainForm.py
+++ api/mainForm.py
@@ -2,36 +2,37 @@
 
 # Form implementation generated from reading ui file 'main.ui'
 #
-# Created by: PyQt5 UI code generator 5.13.0
+# Created by: PyQt5 UI code generator 5.9.2
 #
 # WARNING! All changes made in this file will be lost!
 
-
 from PyQt5 import QtCore, QtGui, QtWidgets
-
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(674, 577)
+        MainWindow.resize(674, 673)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.our_map = QtWidgets.QLabel(self.centralwidget)
         self.our_map.setGeometry(QtCore.QRect(10, 10, 650, 450))
         self.our_map.setText("")
         self.our_map.setObjectName("our_map")
-        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(30, 470, 81, 31))
-        self.pushButton.setObjectName("pushButton")
-        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_2.setGeometry(QtCore.QRect(120, 470, 81, 31))
-        self.pushButton_2.setObjectName("pushButton_2")
-        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_3.setGeometry(QtCore.QRect(210, 470, 81, 31))
-        self.pushButton_3.setObjectName("pushButton_3")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(10, 590, 211, 31))
+        self.label.setObjectName("label")
+        self.input = QtWidgets.QLineEdit(self.centralwidget)
+        self.input.setGeometry(QtCore.QRect(10, 510, 341, 31))
+        self.input.setObjectName("input")
+        self.search = QtWidgets.QPushButton(self.centralwidget)
+        self.search.setGeometry(QtCore.QRect(10, 550, 341, 31))
+        self.search.setObjectName("search")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(10, 470, 211, 31))
+        self.label_2.setObjectName("label_2")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 674, 21))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 674, 26))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
@@ -44,6 +45,7 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.pushButton.setText(_translate("MainWindow", "Спутник"))
-        self.pushButton_2.setText(_translate("MainWindow", "Карта"))
-        self.pushButton_3.setText(_translate("MainWindow", "Схема"))
+        self.label.setText(_translate("MainWindow", "press M to change type of the map"))
+        self.search.setText(_translate("MainWindow", "Искать"))
+        self.label_2.setText(_translate("MainWindow", "Введите адрес:"))
+
