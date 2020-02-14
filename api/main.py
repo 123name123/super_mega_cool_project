3fea65965a5d5fb0d63691ffb48d1882fd128e28
MaximZobkov <46843651+MaximZobkov@users.noreply.github.com>
2020-02-14 23:03:58 +0300

    clear_search

--- api/main.py
+++ api/main.py
@@ -4,7 +4,6 @@ import requests
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QMainWindow
-
 from mainForm import Ui_MainWindow
 
 
@@ -13,16 +12,39 @@ class Example(QMainWindow, Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
         self.initUI()
-        self.pushButton.clicked.connect(self.map_chng)
 
     def initUI(self):
+        self.search.clicked.connect(self.run_search)
+        self.chng_map.clicked.connect(self.map_chng)
+        self.pushButton.clicked.connect(self.del_search)
         self.dest_num = 1
         self.dest_list = [0.002, 0.005, 0.02, 0.05, 0.1, 0.3, 0.5, 1, 3, 5, 11, 15, 40]
         self.shir_ch = 37
         self.dol_ch = 55
         self.map = 'map'
+
+    def run_search(self):
+        toponym_to_find = self.input.text()
+        geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+        geocoder_params = {
+            "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+            "geocode": toponym_to_find,
+            "format": "json"}
+        response = requests.get(geocoder_api_server, params=geocoder_params)
+        if not response:
+            return
+        json_response = response.json()
+        toponym = json_response["response"]["GeoObjectCollection"][
+            "featureMember"][0]["GeoObject"]
+        self.shir_ch, self.dol_ch = toponym["Point"]["pos"].split()
+        self.shir_ch, self.dol_ch = float(self.shir_ch), float(self.dol_ch)
         self.run_start()
 
+    def del_search(self):
+        self.input.clear()
+        self.shir_ch, self.dol_ch = 57, 37
+        self.run_start()
+        
     def map_chng(self):
         if self.map == 'map':
             self.map = 'sat'
@@ -53,6 +75,8 @@ class Example(QMainWindow, Ui_MainWindow):
         elif event.key() == Qt.Key_Left:
             self.shir_ch -= self.dest_list[self.dest_num]
             self.run_start()
+        elif event.key() == Qt.Key_M:
+            self.map_chng()
 
     def run_start(self):
         try:
@@ -60,7 +84,8 @@ class Example(QMainWindow, Ui_MainWindow):
             dest = self.dest_list[self.dest_num]
             map_request = f"http://static-maps.yandex.ru/1.x/?" \
                           f"ll={self.shir_ch},{self.dol_ch}&spn" \
-                          f"={dest},{dest}&l={self.map}&size=650,450"
+                          f"={dest},{dest}&l={self.map}&size=650,450" \
+                          f"&pt={self.shir_ch},{self.dol_ch}"
             response = requests.get(map_request)
             if self.map == 'sat':
                 self.map_file = "map.jpg"
